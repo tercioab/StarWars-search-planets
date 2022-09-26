aa09e8825bf3632e0143e659689847e1a44b60dc
walthercio Almeida <suaagenciainicial@gmail.com>
2022-09-26 02:29:54 -0300

    move function setFilter to FilterByname

--- src/components/forms/filterByName.js
+++ src/components/forms/filterByName.js
@@ -2,8 +2,16 @@ import React, { useContext } from 'react';
 import myContext from '../../context/context';
 
 function FilterByName() {
-  const { functions } = useContext(myContext);
-  const { setFilterNameChange } = functions;
+  const { setStates } = useContext(myContext);
+  const { setFilterByName } = setStates;
+
+  const setFilterNameChange = ({ target }) => {
+    const { value, name } = target;
+    setFilterByName({
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <input
